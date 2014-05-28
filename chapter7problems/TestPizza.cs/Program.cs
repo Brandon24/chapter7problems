1168ea6d30ee600f951c25ae769adf691e543614
Brandon24 <brandonreinert24@gmail.com>
2014-05-28 10:07:54 -0400

    Golf

---
+++ chapter7problems/TestPizza.cs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestPizza.cs
+{
+    class Program
+    {
+        private double radius;
+        //constructor
+        public pie(double diameter)
+        {
+            this.radius = diameter12;
+        }
+        public double radius
+        {
+            get
+            {
+                return radius;
+            }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ chapter7problems/chapter7problems/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace chapter7problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
